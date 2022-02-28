bfe71c573126b893ae62d4ba35ffd1931af48074
tmundz <tmundy2012@gmail.com>
2022-02-28 00:48:15 -0700

    Users can now message other users

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use tokio::{net::TcpListener, 
-    io::{AsyncReadExt, AsyncWriteExt, BufReader, AsyncBufReadExt} 
+    io::{AsyncWriteExt, BufReader, AsyncBufReadExt}, sync::broadcast 
 
 };
+use colored::*;
 
 
 #[tokio::main]
@@ -10,30 +11,48 @@ async fn main() {
     
     //tcp  listener .await will suspend current task until needed
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();  
+
+    //this will allow users to send messages to other users 
+    //specifically tells the compiler they are sending strings
+    let (txt, _rec) = broadcast::channel(10);
     
 
    
-
+    //this loop will wait till someone connects to the server 
     loop {
-        let ( mut socket, _addr) = listener.accept().await.unwrap();
+        let txt = txt.clone(); //moves txt to the new task 
+        let mut rec = txt.subscribe(); //pulls the reciever from the sender
+        let ( mut socket, addr) = listener.accept().await.unwrap();
+
+        //this will create a new task with tokio spawn
         tokio::spawn (async move {
             let (reader, mut writer) = socket.split();
 
             let mut reader = BufReader::new(reader);
             let mut line = String::new();
         
-    
+            //this loop read the bytes that a user sends 
             loop {
-                    
-                    let bytes_read = reader.read_line(&mut line).await.unwrap();
-                    if bytes_read == 0 {
-                        break; 
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                    if result.unwrap() == 0 {
+                       break;
                     }
-                    //send back 
-                    writer.write_all(line.as_bytes()).await.unwrap(); // writes back to the client
+                    txt.send((line.clone(), addr)).unwrap();
+                    
                     line.clear();
-            }
-    });
+                }
+                    result = rec.recv() => {
 
+                        let (msg, other_addr) = result.unwrap();
+                      
+                        if addr != other_addr {
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
+                    
+                }
+            }   
+        });
     }
 }
